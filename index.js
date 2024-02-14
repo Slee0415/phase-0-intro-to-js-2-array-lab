f38b4afd703ddabb6195b409aedfcbd94ede1e8f
Shane Lee <slee0415@gmail.com>
2024-02-14 00:12:46 -0500

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+  cats.push(name);
+}
+
+module.exports = {
+  destructivelyAppendCat
+}
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+  }
+  
+  module.exports = {
+    destructivelyPrependCat
+  }
+  function destructivelyRemoveLastCat() {
+    cats.pop();
+  }
+  
+  module.exports = {
+    destructivelyRemoveLastCat
+  }
+  function destructivelyRemoveFirstCat() {
+    cats.shift();
+  }
+  
+  module.exports = {
+    destructivelyRemoveFirstCat
+  }
+  function appendCat(name) {
+    const newCats = [...cats, name];
+    return newCats;
+  }
+  
+  module.exports = {
+    appendCat
+  }
+  function prependCat(name) {
+    const newCats = [name, ...cats];
+    return newCats;
+  }
+  
+  module.exports = {
+    prependCat
+  }
+  function removeLastCat() {
+    const newCats = cats.slice(0, -1);
+    return newCats;
+  }
+  
+  module.exports = {
+    removeLastCat
+  }
+  function removeFirstCat() {
+    const newCats = cats.slice(1);
+    return newCats;
+  }
+  
+  module.exports = {
+    removeFirstCat
+  };
